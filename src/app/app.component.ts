deed6c2ec64f7641d7c52bc1d37963c220a56953
DIAKITE <mdiakite.pro@gmail.com>
2019-02-01 16:21:25 +0100

    creation appreil component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mahamadou ;-)';
+  title = 'le premier angular de mahamadou';
 }
